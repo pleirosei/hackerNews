d9c43719fa73c6f431bf8bd3c551983c80dc61be
Sean Livingston <sean.d.livingston@gmail.com>
2015-01-17 00:08:03 -0500

    adds

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 angular.module('hackerNews', ['ui.router'])
+	.config(['$stateProvider', 'urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('home', {
+					url: '/home',
+					templateUrl: '/home.html',
+					controller: 'MainCtrl'
+				});
+
+			$urlRouterProvider.otherwise('home');
+	}])
 	.factory('posts', [function() {
 		var o = {
 			posts: []
