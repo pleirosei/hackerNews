2a4dc0e59ee0d7db2e07218657d39c3bc5227ca9
Sean Livingston <sean.d.livingston@gmail.com>
2015-01-17 15:54:16 -0500

    adds necessary functions to service and updates controller to allow persistent data interaction with database

--- hacker-news/app/assets/javascripts/app.js
+++ hacker-news/app/assets/javascripts/app.js
@@ -5,7 +5,12 @@ angular.module('hackerNews', ['ui.router', 'templates'])
 				.state('home', {
 					url: '/home',
 					templateUrl: 'home/_home.html',
-					controller: 'MainCtrl'
+					controller: 'MainCtrl',
+					resolve: {
+						postPromise: ['posts', function(posts) {
+							return posts.getAll();
+						}]
+					}
 				})
 				.state('posts', {
 					url: '/posts/{id}',
--- hacker-news/app/assets/javascripts/posts/posts.js
+++ hacker-news/app/assets/javascripts/posts/posts.js
@@ -1,7 +1,26 @@
 angular.module('hackerNews')
-.factory('posts', [function() {
+.factory('posts', ['$http',
+	function($http) {
 		var o = {
 			posts: []
+		};
+
+		o.getAll = function() {
+			return $http.get('/posts.json').success(function(data) {
+				angular.copy(data, o.posts);
+			});
+		};
+
+		o.create = function(post) {
+			return $http.post('/posts.json', post).success(function(data) {
+				o.posts.push(data);
+			});
+		};
+
+		o.upvote = function(post) {
+			return $http.put('/posts/' + post.id + '/upvote.json').success(function(data) {
+				post.upvotes += 1;
+			});
 		};
 		return o;
 }])
